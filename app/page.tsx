76ce2d442382efc787b5b7f6fc0f57fe0ff51325
Kaiichiro <kigh.ota@gmail.com>
2024-02-18 22:29:29 +0900

    Add some links

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,43 @@
+import styles from './page.module.css';
+
 export default function Home() {
   return (
     <>
-      <header>
-        <h1>Kaiichiro Ota</h1>
-      </header>
-      <main>Hello</main>
+      <main>
+        Hello
+        <div className={styles.container}>
+          <section className={styles.section}>
+            <h1>Kaiichiro Ota</h1>
+            <ul>
+              <li>
+                <a href="https://github.com/kigh-ota" target="_blank">
+                  GitHub (kigh-ota)
+                </a>
+              </li>
+              <li>
+                <a href="https://x.com/kigh" target="_blank">
+                  X (@kigh)
+                </a>
+              </li>
+              <li>
+                <a href="https://kigh-memo.hatenablog.com/" target="_blank">
+                  Hatena Blog
+                </a>
+              </li>
+              <li>
+                <a href="https://speakerdeck.com/kaiichiro" target="_blank">
+                  Speaker Deck
+                </a>
+              </li>
+            </ul>
+          </section>
+          <section className={styles.section}>Section 2</section>
+          <section className={styles.section}>Section 3</section>
+          <section className={styles.section}>Section 4</section>
+          <section className={styles.section}>Section 5</section>
+          <section className={styles.section}>Section 6</section>
+        </div>
+      </main>
     </>
   );
 }
